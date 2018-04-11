995b74d0a22a59f76de1ab9dc899ca6002ad8ce1
cardona150 <575891+cardona150@users.noreply.github.com>
2018-04-10 18:18:23 -0700

    Fixed itemScript for cases where nobody is offering to sell/buy an item

--- extension/itemScript.js
+++ extension/itemScript.js
@@ -1,7 +1,13 @@
-highestBuyOrder = cleanNumberString($(".marketplace-card-sell-orders").find("td")[0].innerText);
-$("#create-buy-order-form").find("#price")[0].value = ++highestBuyOrder;
-lowestSellOrder = cleanNumberString($(".marketplace-card-buy-orders").find("td")[0].innerText);
-$("#create-sell-order-form").find("#price")[0].value = --lowestSellOrder;
+highestBuyOrderContainer = $(".marketplace-card-sell-orders").find("td")[0];
+if (highestBuyOrderContainer != null) {
+    highestBuyOrderNumber = cleanNumberString(highestBuyOrderContainer.innerText);
+    $("#create-buy-order-form").find("#price")[0].value = ++highestBuyOrderNumber;
+}
+lowestSellOrderContainer = $(".marketplace-card-buy-orders").find("td")[0];
+if (lowestSellOrderContainer != null) {
+    lowestSellOrder = cleanNumberString(lowestSellOrderContainer.innerText);
+    $("#create-sell-order-form").find("#price")[0].value = --lowestSellOrder;
+}
 
 function cleanNumberString(numberString) {
     return Number(numberString.replace(/\D/g, ""));
