b9be1ad3346837256dbadaab05eb5bdea46310aa
cardona150 <575891+cardona150@users.noreply.github.com>
2018-04-12 13:21:42 -0700

    Rename some variable more accurately and start  using moment.js
    Some variables were named wrong. I'm also bad at parsing dates so just use moment.js for this.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Get it at https://chrome.google.com/webstore/detail/mlb-the-show-18-market-as/agepfhhdmecofdlnkeafaioilekmdmlh
 
-Last I measured this helped me make 4000 stubs/hour using 10,000 stubs in capital.
+Last I measured this helped me make 5000 stubs/hour using 10,000 stubs in capital.
--- extension/scripts/listingsPageScript.js
+++ extension/scripts/listingsPageScript.js
@@ -1,7 +1,7 @@
 'use strict';
 // "Market prices on this page are refreshed hourly." Not anymore!!! :D
 $('.marketplace-filter-disclaimer').remove();
-var tableHTML = '<table class="sortable" width=100%; style="font-size:15px;"><thead style="background-color:#151515;font-weight:bold;"><tr><th>Name</th><th class="sorttable_numeric">Buy</th><th class="sorttable_numeric">Sell</th><th class="sorttable_numeric">Profit(After Tax)</th><th class="sorttable_numeric">Difference% (After Tax)</th><th class="sorttable_numeric">Bought/Sold in the past hour</th></thead></tr><tbody>';
+var tableHTML = '<table class="sortable" width=100%; style="font-size:15px;"><thead style="background-color:#151515;font-weight:bold;"><tr><th>Name</th><th class="sorttable_numeric">Buy</th><th class="sorttable_numeric">Sell</th><th class="sorttable_numeric">Profit(After Tax)</th><th class="sorttable_numeric">Difference% (After Tax)</th><th class="sorttable_numeric">Traded in the past hour</th></th><th class="sorttable_numeric">Profit per hour</th></tr></thead><tbody>';
 var objectArray = [];
 $('.marketplace-filter-item').each(function () {
     var name = $(this).find('.marketplace-filter-item-name > a').text();
@@ -15,7 +15,7 @@ $('.marketplace-filter-item').each(function () {
 // Thanks for the links! Goodbye!
 $('.marketplace-filter-list').remove();
 $(objectArray).each(function () {
-    tableHTML += '<tr><td class="nameRow"><a href="' + $(this)[0].url + '">' + $(this)[0].name + '</a></td><td class="buyNowRow">-</td><td class="sellNowRow">-</td><td class="datRow">-</td><td class="pdRow">-</td><td class="soldInHourRow">-</td></tr>';
+    tableHTML += '<tr><td class="nameRow"><a href="' + $(this)[0].url + '">' + $(this)[0].name + '</a></td><td class="buyNowRow">-</td><td class="sellNowRow">-</td><td class="datRow">-</td><td class="pdRow">-</td><td class="tradedPastHour">-</td><td class="profitPerHour">-</td></tr>';
 });
 tableHTML += '</tbody></table>';
 $('.menu-pagination').before('<script type="text/javascript" src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js">');
@@ -23,13 +23,14 @@ $('.menu-pagination').before(tableHTML);
 $('.sortable > tbody  > tr').each(function () {
     var buyPrice;
     var sellPrice;
-    var mSoldInHour = 0;
+    var tradedPastHour = 0;
     var rowURL = $(this).find('.nameRow > a').attr('href');
     var buyNowRow = $(this).find('.buyNowRow');
     var sellNowRow = $(this).find('.sellNowRow');
     var datRow = $(this).find('.datRow');
     var pdRow = $(this).find('.pdRow');
-    var soldInHourRow = $(this).find('.soldInHourRow');
+    var tradedPastHourRow = $(this).find('.tradedPastHour');
+    var profitPerHourRow = $(this).find('.profitPerHour');
     $.ajax({
         url: rowURL
     }).done(function (data) {
@@ -46,51 +47,34 @@ $('.sortable > tbody  > tr').each(function () {
         // Don't have to do this but it makes console errors go away
         if (sellPrice != null) { sellNowRow.html(sellPrice); }
         parsedData.find(".completed-order").each(function () {
-            var thisDate = getMlbtsDateFromString($(this).find(".date").text());
+            console.log($(this).find(".date").text());
+            var thisDate = moment($(this).find(".date").text(), "MMMM DD, YYYY hh:mA").toDate();
             var OneHourAgo = new Date(Date.now());
             OneHourAgo.setHours(OneHourAgo.getHours() - 1);
             // These come in descending order
             if (thisDate > OneHourAgo) {
-                mSoldInHour++;
+                tradedPastHour++;
             }
             else {
                 // Stop as soon as you find one that is more than an hour ago
                 return false;
             }
         });
-        soldInHourRow.html(mSoldInHour);
+        tradedPastHourRow.html(tradedPastHour);
         // buyPrice can be null if nobody is offering to sell this item
         // sellPrice can be null if nobody is offering to buy this item
         if (buyPrice != null && sellPrice != null) {
-            var profit = (sellPrice*.9) - buyPrice;
-            profit = precisionRound(profit, 2);
+            var profit = (sellPrice * .9) - buyPrice;
+            profit = precisionRound(profit, 0);
             datRow.html(profit);
             var percentDifference = (profit / buyPrice) * 100;
             percentDifference = precisionRound(percentDifference, 2);
             pdRow.html(percentDifference + "%");
+            var profitPerHour = profit * (tradedPastHour / 2);
+            profitPerHourRow.html(precisionRound(profitPerHour, 0));
         }
     });
 });
-
-// Chrome can't convert the date format in the Completed Orders section of an item's page into a Date
-// This metod will reformat the string properly and the turn it into a Date object
-function getMlbtsDateFromString(dateString) {
-    var date;
-    var fixedDateString;
-    if (dateString.substring(20, 21) == "P") {
-        // If PM, add 12 hours and cut off the end bit that keeps dateString from deserializing properly
-        var hour = parseInt(dateString.substring(15, 17));
-        hour += 12;
-        fixedDateString = dateString.slice(0, 15) + hour + dateString.slice(17, 20);
-        date = new Date(fixedDateString);
-    }
-    else {
-        // If AM just cut off the end bit that keeps dateString from deserializing properly
-        fixedDateString = dateString.slice(0, 20);
-        date = new Date(fixedDateString);
-    }
-    return date;
-}
 
 function precisionRound(number, precision) {
     var factor = Math.pow(10, precision);
