cd2e6e1f0537d62998a2fc5080171e9f88acc63b
cardona150 <575891+cardona150@users.noreply.github.com>
2018-04-12 21:10:53 -0700

    itemPage improvements
    Swapped tables (why did they put them backwards?!). Removed alerts and placed cancel buttons instead.

--- extension/scripts/itemPageScript.js
+++ extension/scripts/itemPageScript.js
@@ -1,9 +1,26 @@
-highestBuyOrderContainer = $(".marketplace-card-sell-orders").find("td")[0];
+$($.find(".marketplace-card-order-now")).each(function (index, element) {
+    // Buy/Sell now buttons will always make you lose money, get them out of the way
+    $(this).remove();
+});
+
+// They put buy orders under the sell header...
+buyOrders = $(".marketplace-card-sell-orders .marketplace-card-open-orders");
+// and sell orders under the buy header...
+sellOrders = $(".marketplace-card-buy-orders .marketplace-card-open-orders");
+// Flip the tables
+temp1 = $('<span>').hide();
+temp2 = $('<span>').hide();
+buyOrders.before(temp1);
+sellOrders.before(temp2);
+temp1.replaceWith(sellOrders);
+temp2.replaceWith(buyOrders);
+
+highestBuyOrderContainer = $(".marketplace-card-buy-orders").find("td")[0];
 if (highestBuyOrderContainer != null) {
     highestBuyOrderNumber = cleanNumberString(highestBuyOrderContainer.innerText);
     $("#create-buy-order-form").find("#price")[0].value = highestBuyOrderNumber + 1;
 }
-lowestSellOrderContainer = $(".marketplace-card-buy-orders").find("td")[0];
+lowestSellOrderContainer = $(".marketplace-card-sell-orders").find("td")[0];
 if (lowestSellOrderContainer != null) {
     lowestSellOrderNumber = cleanNumberString(lowestSellOrderContainer.innerText);
     $("#create-sell-order-form").find("#price")[0].value = lowestSellOrderNumber - 1;
@@ -23,12 +40,13 @@ $.ajax({
         if (itemPath == buyOrderPath) {
             buyOrderPriceContainer = $(element).find(".item-price")[0];
             buyOrderPrice = cleanNumberString($(buyOrderPriceContainer)[0].innerText);
-            if (buyOrderPrice != highestBuyOrderNumber) {
-                alert("You are offering to buy for " + buyOrderPrice + " but there is a better offer: " + highestBuyOrderNumber);
-            }
-            else {
-                alert("You are offering to buy for the best price price: " + buyOrderPrice);
-            }
+            cancelButton = $(this).find(".item-cancel");
+            $(".marketplace-card-buy-orders .marketplace-card-open-orders tbody tr").each(function (index) {
+                currentRowPrice = cleanNumberString($($(this)[0].children[0])[0].innerText);
+                if (buyOrderPrice == currentRowPrice) {
+                    $($(this)[0].children[1])[0].innerHTML = cancelButton[0].innerHTML;
+                }
+            });
         }
     });
 
@@ -39,12 +57,13 @@ $.ajax({
         if (itemPath == sellOrderPath) {
             sellOrderPriceContainer = $(element).find(".item-price")[0];
             sellOrderPrice = cleanNumberString($(sellOrderPriceContainer)[0].innerText);
-            if (sellOrderPrice != lowestSellOrderNumber) {
-                alert("You are offering to sell for " + sellOrderPrice + " but there is a better offer: " + lowestSellOrderNumber);
-            }
-            else {
-                alert("You are offering to sell for the best price: " + sellOrderPrice);
-            }
+            cancelButton = $(this).find(".item-cancel");
+            $(".marketplace-card-sell-orders .marketplace-card-open-orders tbody tr").each(function (index) {
+                currentRowPrice = cleanNumberString($($(this)[0].children[0])[0].innerText);
+                if (sellOrderPrice == currentRowPrice) {
+                    $($(this)[0].children[1])[0].innerHTML = cancelButton[0].innerHTML;
+                }
+            });
         }
     });
 });
