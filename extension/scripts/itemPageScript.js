187026d0e8a57b6247097dcb3e8d0fd055dddffc
cardona150 <575891+cardona150@users.noreply.github.com>
2018-04-14 03:54:29 -0700

    Restyle itemPages
    Remove non-trade related info from itemPage. Remove captcha?

--- extension/scripts/itemPageScript.js
+++ extension/scripts/itemPageScript.js
@@ -1,8 +1,7 @@
+// Buy/Sell now buttons will always make you lose money, get them out of the way
 $($.find(".marketplace-card-order-now")).each(function (index, element) {
-    // Buy/Sell now buttons will always make you lose money, get them out of the way
     $(this).remove();
 });
-
 // They put buy orders under the sell header...
 buyOrders = $(".marketplace-card-sell-orders .marketplace-card-open-orders");
 // and sell orders under the buy header...
@@ -14,20 +13,36 @@ buyOrders.before(temp1);
 sellOrders.before(temp2);
 temp1.replaceWith(sellOrders);
 temp2.replaceWith(buyOrders);
-
-$($.find(".marketplace-card-info")).remove();
-
+// Set background image
+$(".marketplace-card-info-asset img").each(function (indexInArray, value) {
+    imgSrc = $(value)[0].src;
+    // Find the actual item image (i.e. instead of the topps or team logos)
+    if (imgSrc.indexOf("actionshots") >= 0 || imgSrc.indexOf("items") >= 0) {
+        $('.marketplace-card').css('background-image', 'url("' + imgSrc + '")');
+    }
+});
+// This must go after setting the background image since $(".marketplace-card-info-asset img") is under $(".marketplace-card-info")
+$(".marketplace-card-info").remove();
+// If we can sell this, tell us
+sellableCount = cleanNumberString($(".marketplace-card-owned")[1].innerHTML);
+// Remove captchas (don't know if this actually works yet)
+$(".g-recaptcha").removeClass("g-recaptcha")
+// Most useful for when you are monitoring a buy order, it sells and the you refresh and see this
+if (sellableCount != 0) {
+    alert("You can sell this");
+}
+// Pre-fill buy order form with the best offer
 highestBuyOrderContainer = $(".marketplace-card-buy-orders").find("td")[0];
 if (highestBuyOrderContainer != null) {
     highestBuyOrderNumber = cleanNumberString(highestBuyOrderContainer.innerText);
     $("#create-buy-order-form").find("#price")[0].value = highestBuyOrderNumber + 1;
 }
+// Pre-fill sell order form with the best offer
 lowestSellOrderContainer = $(".marketplace-card-sell-orders").find("td")[0];
 if (lowestSellOrderContainer != null) {
     lowestSellOrderNumber = cleanNumberString(lowestSellOrderContainer.innerText);
     $("#create-sell-order-form").find("#price")[0].value = lowestSellOrderNumber - 1;
 }
-
 breadcrumbsContainer = $(".breadcrumbs > a");
 itemPath = $(breadcrumbsContainer[2])[0].pathname;
 $.ajax({
