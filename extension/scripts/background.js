f52d787f3871f84db0fa3a9e16b6eeef870c88bc
cardona150 <575891+cardona150@users.noreply.github.com>
2018-04-17 22:08:59 -0700

    Small things for store

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function (activeTab) {
-    chrome.tabs.create({ url: 'https://mlb18.theshownation.com/community_market' });
+    chrome.tabs.create({url: 'https://mlb18.theshownation.com/community_market'});
 });
