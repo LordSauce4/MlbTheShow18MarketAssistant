deb150929d46ea45cb7c6cd3982cb9fe0cc0d3b8
cardona150 <575891+cardona150@users.noreply.github.com>
2018-04-19 18:20:58 -0700

    Nice action buttons

--- extension/scripts/itemPageScript.js
+++ extension/scripts/itemPageScript.js
@@ -32,12 +32,15 @@ buyOrders = $(".marketplace-card-sell-orders .marketplace-card-open-orders");
 // and sell orders under the buy header...
 sellOrders = $(".marketplace-card-buy-orders .marketplace-card-open-orders");
 // Flip the tables
-temp1 = $('<span>').hide();
-temp2 = $('<span>').hide();
+temp1 = $("<span>").hide();
+temp2 = $("<span>").hide();
 buyOrders.before(temp1);
 sellOrders.before(temp2);
 temp1.replaceWith(sellOrders);
 temp2.replaceWith(buyOrders);
+// Add Actions column
+$("thead tr").append("<th>Action</th>");
+$("tbody tr").append("<td></td>");
 // Do this after the flip
 // Pre-fill buy order form with the best offer
 highestBuyOrderContainer = $(".marketplace-card-buy-orders td")[0];
@@ -54,7 +57,6 @@ if (lowestSellOrderContainer != null) {
 // Calculate profit and write it in our a new heading
 profitPercentAfterTax = getProfitPercentAfterTax(highestBuyOrderNumber, lowestSellOrderNumber);
 clone[0].innerText += " | Profit%(After Tax): " + profitPercentAfterTax.toString() + "%";
-
 // Calculate profit/hr and write it in our a new heading
 clone[0].innerText += " | Profit%(After Tax)/hour: " + getProfitPercentAfterTaxPerHour(profitPercentAfterTax, tradedPerHour).toString() + "%";
 // Set background image
@@ -67,42 +69,56 @@ $(".marketplace-card-info-asset img").each(function (indexInArray, value) {
 });
 // This must go after setting the background image since $(".marketplace-card-info-asset img") is under $(".marketplace-card-info")
 $(".marketplace-card-info").remove();
+// Get the identifier for this item
 breadcrumbsContainer = $(".breadcrumbs > a");
 itemPath = $(breadcrumbsContainer[2])[0].pathname;
 $.ajax({
     url: "/orders"
 }).done(function (data) {
-    // TODO: context thing https://api.jquery.com/jquery.parsehtml/
     var parsedData = $(jQuery.parseHTML(data));
     shopList = parsedData.find(".shop-list");
     buyOrders = $(shopList).find(".orders")[0];
-    $(buyOrders).find(".order").each(function (index, element) {
-        buyOrderPathContainer = $(element).find("a")[0];
+    // Go through all our open buy orders
+    $(buyOrders).find(".order").each(function () {
+        buyOrderPathContainer = $(this).find("a")[0];
         buyOrderPath = buyOrderPathContainer.pathname;
+        // If we have an open buy order for the item we are currently viewing
         if (itemPath == buyOrderPath) {
-            buyOrderPriceContainer = $(element).find(".item-price")[0];
+            buyOrderPriceContainer = $(this).find(".item-price")[0];
+            // Remember how many stubs this open buy order is for
             buyOrderPrice = cleanNumberString($(buyOrderPriceContainer)[0].innerText);
-            cancelButton = $(this).find(".item-cancel");
-            $(".marketplace-card-buy-orders .marketplace-card-open-orders tbody tr").each(function (index) {
+            cancelButton = $(this).find("form");
+            // Go through every buy order for this item
+            $(".marketplace-card-buy-orders tbody tr").each(function () {
                 currentRowPrice = cleanNumberString($($(this)[0].children[0])[0].innerText);
+                // If our open buy order is in this list
                 if (buyOrderPrice == currentRowPrice) {
-                    $($(this)[0].children[1])[0].innerHTML = cancelButton[0].innerHTML;
+                    // TODO: Add repost button $("#create-buy-order-form").submit()
+                    // Place the corresponding cancel button in this row
+                    $(this).children()[2].append(cancelButton[0]);
                 }
             });
         }
     });
     sellOrders = $(shopList).find(".orders")[1];
-    $(sellOrders).find(".order").each(function (index, element) {
-        sellOrderPathContainer = $(element).find("a")[0];
+    // Go through all of our open sell orders
+    $(sellOrders).find(".order").each(function () {
+        sellOrderPathContainer = $(this).find("a")[0];
         sellOrderPath = sellOrderPathContainer.pathname;
+        // If we have an open sell  order for the item we are currently viewing
         if (itemPath == sellOrderPath) {
-            sellOrderPriceContainer = $(element).find(".item-price")[0];
+            sellOrderPriceContainer = $(this).find(".item-price")[0];
+            // Remember how many stubs this open sell order is for
             sellOrderPrice = cleanNumberString($(sellOrderPriceContainer)[0].innerText);
-            cancelButton = $(this).find(".item-cancel");
-            $(".marketplace-card-sell-orders .marketplace-card-open-orders tbody tr").each(function (index) {
+            cancelButton = $(this).find("form");
+            // Go through every sell order for this item
+            $(".marketplace-card-sell-orders tbody tr").each(function () {
                 currentRowPrice = cleanNumberString($($(this)[0].children[0])[0].innerText);
+                // If our open buy order is in this list
                 if (sellOrderPrice == currentRowPrice) {
-                    $($(this)[0].children[1])[0].innerHTML = cancelButton[0].innerHTML;
+                    // TODO: Add repost button $("#create-buy-order-form").submit()
+                    // Place the corresponding cancel button in this row
+                    $(this).children()[2].append(cancelButton[0]);
                 }
             });
         }
@@ -117,4 +133,5 @@ setTimeout(
         $("#create-buy-order-form").replaceWith(buyOrderFormClone);
         sellOrderFormClone = $("#create-sell-order-form").clone();
         $("#create-sell-order-form").replaceWith(sellOrderFormClone);
-    }, 500);
+    }, 500
+);
